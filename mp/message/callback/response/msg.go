6c1ba3036da3cb7be264b378424011b112dcb744
Laughing_Vzr <LouWazor@gmail.com>
2017-05-10 23:56:01 +0800

    继续调整

--- mp/message/callback/response/msg.go
+++ mp/message/callback/response/msg.go
@@ -138,7 +138,7 @@ func NewMusic(to, from string, timestamp int64, thumbMediaId, musicURL, HQMusicU
 type Article struct {
 	Title       string `xml:"Title,omitempty"       json:"Title,omitempty"`        // 图文消息标题
 	Description string `xml:"Description,omitempty" json:"Description,omitempty"`  // 图文消息描述
-	PicURL      string `xml:"thumb_url,omitempty"      json:"thumb_url,omitempty"` // 图片链接, 支持JPG, PNG格式, 较好的效果为大图360*200, 小图200*200
+	PicUrl      string `xml:"thumb_url,omitempty"      json:"thumb_url,omitempty"` // 图片链接, 支持JPG, PNG格式, 较好的效果为大图360*200, 小图200*200
 	URL         string `xml:"Url,omitempty"         json:"Url,omitempty"`          // 点击图文消息跳转链接
 }
 
